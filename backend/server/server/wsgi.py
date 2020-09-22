f2afdc35e50d6852d4cfb667cb1d966ddb8d05fb
mohamedwaelbenismail <ghaida.bouchaala@gmail.com>
2020-09-22 16:49:03 +0100

    ab tests

--- backend/server/server/wsgi.py
+++ backend/server/server/wsgi.py
@@ -1,21 +1,4 @@
-"""
-WSGI config for server project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
-"""
-
-import os
-
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'server.settings')
-
-application = get_wsgi_application()
-
-
+# the `backend/server/server/wsgi.py file
 import os
 from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'server.settings')
@@ -25,6 +8,7 @@ application = get_wsgi_application()
 import inspect
 from apps.ml.registry import MLRegistry
 from apps.ml.income_classifier.random_forest import RandomForestClassifier
+from apps.ml.income_classifier.extra_trees import ExtraTreesClassifier # import ExtraTrees ML algorithm
 
 try:
     registry = MLRegistry() # create ML registry
@@ -40,5 +24,16 @@ try:
                             algorithm_description="Random Forest with simple pre- and post-processing",
                             algorithm_code=inspect.getsource(RandomForestClassifier))
 
+    # Extra Trees classifier
+    et = ExtraTreesClassifier()
+    # add to ML registry
+    registry.add_algorithm(endpoint_name="income_classifier",
+                            algorithm_object=et,
+                            algorithm_name="extra trees",
+                            algorithm_status="testing",
+                            algorithm_version="0.0.1",
+                            owner="Piotr",
+                            algorithm_description="Extra Trees with simple pre- and post-processing",
+                            algorithm_code=inspect.getsource(RandomForestClassifier))
 except Exception as e:
     print("Exception while loading the algorithms to the registry,", str(e))
