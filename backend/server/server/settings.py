889bbbdc6e6cd461429d37666ae7153ca45bce22
mohamedwaelbenismail <ghaida.bouchaala@gmail.com>
2020-09-22 15:39:34 +0100

    add ml code

--- backend/server/server/settings.py
+++ backend/server/server/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'apps.endpoints'
+    'apps.endpoints',
+    'apps.ml'
 ]
 
 MIDDLEWARE = [
